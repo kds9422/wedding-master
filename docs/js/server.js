15b07a03e66a4e12dd7d7c25698e47cfc10afebe
kds9422 <kds9422@gmail.com>
2024-03-22 11:36:19 +0900

    Create server.js

---
+++ docs/js/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit-comment', (req, res) => {
+    const comment = req.body.comment;
+
+    fs.appendFile('README.md', '- ' + comment + '\n', (err) => {
+        if (err) throw err;
+        console.log('Comment saved: ' + comment);
+    });
+
+    res.redirect('/');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
